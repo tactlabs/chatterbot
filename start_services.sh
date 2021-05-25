7a23f667bb99c46de0d18adc8ff699a8fae0937a
ajaykumaar <31564988+ajaykumaar@users.noreply.github.com>
2021-05-25 12:32:41 +0530

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,4 @@
 cd app/
 #start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug \
--p $PORT 
+rasa run --model models --enable-api --cors "*" --debug \-p $PORT 
 
